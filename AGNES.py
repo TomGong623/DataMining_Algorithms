5581113bca7cba597407d7e06641bc0d029333b4
tongg2@illinois.edu <tongg2@illinois.edu>
2019-04-29 16:21:59 -0500

    AGNES

--- AGNES.py
+++ AGNES.py
@@ -1,6 +1,9 @@
 import sys
 
 input_data = []
+dists = {}
+clusters = {}
+results = {}
 
 for row in sys.stdin:
     if "\n" in row:
@@ -25,9 +28,42 @@ for i in range(1, N + 1):
         temp.append(float(sample[j]))
     temp.append(i - 1)
     samples[i - 1] = temp
-
-for x in samples:
-    print(x)
+    clusters[i - 1] = [i - 1]
 
 for i in range(N):
-    for j in range(i, N):
+    for j in range(i + 1, N):
+        dist = 0
+        for z in range(D):
+            diff = (samples[i][z] - samples[j][z]) ** 2
+            dist += diff
+        dists[dist] = [i, j]
+
+while (len(clusters) > k):
+    rm = None
+    Min = list(dists.keys())[0]
+    for dist in dists:
+        if (dist < Min):
+            Min = dist
+    head = dists[Min][0]
+    link = dists[Min][1]
+    for c in clusters:
+        if (head in clusters[c] and link not in clusters[c]):
+            for d in clusters:
+                if (link in clusters[d]):
+                    clusters[c] = clusters[c] + clusters[d]
+                    rm = d
+    if (rm != None):
+        clusters.pop(rm)
+    dists.pop(Min)
+
+for x in clusters:
+    Min = clusters[x][0]
+    for y in clusters[x]:
+        if (y < Min):
+            Min = y
+    results[Min] = clusters[x]
+
+for x in samples:
+    for c in results:
+        if (x[-1] in results[c]):
+            print(c)
